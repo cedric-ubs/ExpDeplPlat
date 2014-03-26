eec37124029ccd7e02722733a907259eca242fec
cedric.seguin_WS <cedric.seguin@univ-ubs.fr>
2014-03-26 14:27:10 +0100

    Modif CmdSender method (runtime) + Add getResCommand

--- src/main/java/fr/univubs/labsticc/expdeplplat/MappingApp.java
+++ src/main/java/fr/univubs/labsticc/expdeplplat/MappingApp.java
@@ -1,7 +1,9 @@
 package fr.univubs.labsticc.expdeplplat;
 
 import com.jcraft.jsch.JSchException;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Random;
@@ -15,10 +17,11 @@ public class MappingApp {
      * I can be a couple TASK/RES
      * but also a simple TASK
      */
-    private static final String TASK = "__tsk_AllumerLumiere";
-
+    private static final String TASK = "_TSK_BigBunnyVideo";
+    private static final String subTASK[] = {"_TSK_BBVClient", "_TSK_BBVServer"};
+    
     /* This is the ontology */
-    private static final String ONTOLOGY = "/home/cedric/ontologies/examples/exp_lampes_rdf.owl";
+    private static final String ONTOLOGY = "/home/seguin/ontologies/examples/exp_lampes_rdf.owl";
 
     private final Path fFilePath;
 
@@ -36,8 +39,10 @@ public class MappingApp {
         /* Perform assignation algorithm to chose the best placement */
         String Resource = performPlacementAlgorithm(mapper.fFilePath, AbleResTab);
         /* Execute the task on the Environment */
-        CommandSender sender = new CommandSender(ONTOLOGY, Resource, TASK);
-        
+        //CommandSender sender = new CommandSender(ONTOLOGY, Resource, TASK);
+        String Command = getResCommand(Resource);
+        RuntimeCmdSender(Command);
+        log("Service executed on "+Resource);
     }
 
     /**
@@ -59,7 +64,7 @@ public class MappingApp {
          * Tab is manually filled but 
          * it should be obtained from ONTO
          */
-        String AvailableResTab[] = {"_lampeEIB1", "_lampeEIB2", "_TVNSX"};
+        String AvailableResTab[] = {"_RES_Pc1", "_RES _Pc2", "_RES_AlaCon"};
         //TabScanning(AvailableResTab);
         return AvailableResTab;
     }
@@ -77,7 +82,7 @@ public class MappingApp {
          * it should be obtained from ONTO
          */
         //String AbleResTab[] = new String[3]; // 3 Resources Max Available
-        String AbleResTab[] = {"_lampeEIB1", "_lampeEIB2"};
+        String AbleResTab[] = {"_RES_Pc1", "_RES_Pc2"};
         //TabScanning(AbleResTab);
         return AbleResTab;
     }
@@ -98,6 +103,66 @@ public class MappingApp {
         int idx = new Random().nextInt(AbleResTab.length);
         String random = (AbleResTab[idx]);
         return random;
+    }
+    
+    private static String getResCommand(String resource){
+        if ("_RES_Pc1".equals(resource)){
+            return "ssh seguin@192.168.100.1 gst-launch-0.10 filesrc location=/home/seguin/bigbunny/big_buck_bunny_1080p_stereo.ogg ! oggdemux ! queue ! theoradec ! xvimagesink display=:0";
+        } else if ("_RES_Pc2".equals(resource)){
+            return "ssh cedric@192.168.100.2 gst-launch-0.10 filesrc location=/home/cedric/igepv2/bigBunny/big_buck_bunny_1080p_stereo.ogg ! oggdemux ! queue ! theoradec ! xvimagesink display=:0";
+        } else {
+            log("Unknown resource"+ resource);
+            return null;
+        }
+    }
+    
+    private static void RuntimeCmdSender(String command) throws IOException {
+        Runtime runtime = Runtime.getRuntime();
+        try {
+            final Process process = runtime.exec(command);
+            /* Consommation de la sortie standard de l'application externe dans un Thread separe */
+            new Thread() {
+                @Override
+                public void run() {
+                    try {
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                        String line = "";
+                        try {
+                            while ((line = reader.readLine()) != null) {
+                                // Traitement du flux de sortie de l'application si besoin est
+                                log("sortie : "+line);
+                            }
+                        } finally {
+                            reader.close();
+                        }
+                    } catch (IOException ioe) {
+                        ioe.printStackTrace();
+                    }
+                }
+            }.start();
+            /* Consommation de la sortie d'erreur de l'application externe dans un Thread separe */
+            new Thread() {
+                @Override
+                public void run() {
+                    try {
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+                        String line = "";
+                        try {
+                            while ((line = reader.readLine()) != null) {
+                                // Traitement du flux d'erreur de l'application si besoin est
+                                log("erreur : "+line);
+                            }
+                        } finally {
+                            reader.close();
+                        }
+                    } catch (IOException ioe) {
+                        ioe.printStackTrace();
+                    }
+                }
+            }.start();
+        } catch (IOException e) {
+        }
+
     }
     
     /* Private method for printing results */
